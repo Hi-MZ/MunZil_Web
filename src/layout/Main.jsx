3acd9d7f0df214a314e49e3018369be6568043fa
bae0203 <gmbae06@dgsw.hs.kr>
2023-01-14 19:25:30 +0900

    ADD :: Main Frame

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,6 +4,12 @@ const Main = () => {
   return (
     <>
       <div>Main</div>
+
+      <div>동영상 받기</div>
+      <p>내 팔로워</p>
+      <div></div>
+      <p>모든 영상</p>
+      <div></div>
     </>
   );
 };

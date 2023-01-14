b80d722136ced77d2c2f7c67596efe88ba842b6a
bae0203 <gmbae06@dgsw.hs.kr>
2023-01-14 19:52:03 +0900

    ADD :: MyInformation
    내 정보창 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [isLoggin, setIsLoggin] = useState(true);
   return (
     <>
       <h2 onClick={() => navigate("/")}>title</h2>
-      <p onClick={() => navigate("/auth")}>로그인</p>
+      {isLoggin ? (
+        <p onClick={() => navigate("/info")}>내 정보</p>
+      ) : (
+        <p onClick={() => navigate("/auth")}>로그인</p>
+      )}
     </>
   );
 };

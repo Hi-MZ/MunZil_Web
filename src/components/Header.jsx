d4b8da02429618ffc11d099432a24c4d11e46ae1
sueeeeh <olivila0501@gmail.com>
2023-01-15 05:05:05 +0900

    Add : Profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,6 @@ import * as S from "../style/HeaderStyle";
 const Header = () => {
   return (
     <>
-      <button
-        onClick={() => {
-          console.log(localStorage.removeItem("Token"));
-        }}
-      >
-        ddd
-      </button>
       <S.MunZilLogo></S.MunZilLogo>
     </>
   );
--- src/layout/Myinfo.jsx
+++ src/layout/Myinfo.jsx
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
+import * as S from "../style/MyinfoStyle";
+import VideoBox from "../components/VideoBox"
+
 const Myinfo = () => {
   const [nickname] = useState("");
   return (
     <>
       <Header></Header>
-      <div>사진</div>
-      <p>{nickname}</p>
-      <nav>
-        <span>
-          <span>팔로워</span>
-          <span>0</span>
-        </span>
-        <span>
-          <span>팔로우</span>
-          <span>3</span>
-        </span>
-      </nav>
+      <S.Align>
+        <S.Title>프로필</S.Title>
+        <S.ChangeBtn>수정</S.ChangeBtn>
+      </S.Align>
+      <S.ProfileSum>
+        <S.Profile></S.Profile>
+        <S.NF>
+        <S.Nic>{nickname}님</S.Nic>
+        <nav>
+          <span>
+            <S.Follow> 팔로워 </S.Follow>
+            <S.Follow> 0  </S.Follow>
+          </span>
+          <span>
+            <S.Follow> 팔로우 </S.Follow>
+            <S.Follow> 3 </S.Follow>
+          </span>
+        </nav>
+        </S.NF>
+      </S.ProfileSum>
+      <hr/>
       <p>내 동영상</p>
-      <div></div>
+      <div>
+        <S.Align>
+          <VideoBox/>
+          <VideoBox/>
+          <VideoBox/>
+        </S.Align>
+        <S.Align>
+          <VideoBox/>
+          <VideoBox/>
+          <VideoBox/>
+        </S.Align>
+      </div>
     </>
   );
 };

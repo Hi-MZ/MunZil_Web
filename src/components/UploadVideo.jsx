0367135685d5fa1d254c1fad703f14e7c9ecbc69
sueeeeh <olivila0501@gmail.com>
2023-01-14 23:26:53 +0900

    ADD : Style

--- src/components/UploadVideo.jsx
+++ src/components/UploadVideo.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useState } from 'react';
 import moment from 'moment'
 import 'moment/locale/ko'
+import * as S from '../style/UploadVideoStyle'
 
 //닉네임(또는 아이디), 오늘 질문 받기
 const UploadVideo = ({id,question}) => {
@@ -28,12 +29,15 @@ const UploadVideo = ({id,question}) => {
     }
 
     return(
+        <>
+        <S.Title>영상 업로드</S.Title>
+        <S.Question>오늘 질문?</S.Question>
         <form onSubmit={onSubmit}>
             <input type="file" name="video" />
             <input name="title" value={title} onChange={onChangeTitle}/>     
             <button type="submit"/>       
         </form>
-
+        </>
     )
 
 }
---
+++ src/style/UploadVideoStyle.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+    font-size: 1.125rem;
+    color: #F5B659;
+    margin-bottom: 1.563rem;
+    margin-left: 1.813rem;
+`
+
+export const Question = styled.div`
+    border: solid ;
+    border-radius: 2rem;
+    width: 22.313rem;
+    height: 3.125rem;
+    background-color: #F5B659;
+    color: #FFFFFF;
+    font-size: 1rem;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 1.063rem;
+    margin-right: 1rem;
+`
+
+export const UploadBtn = styled.div`
+    border: solid white;
+    border-radius: 50%;
+    width: 2.375rem;
+    height: 2.375rem;
+    box-shadow: 0rem 0.2rem 1rem rgba(1,1,1,0.2);
+
+    margin-left: 1.063rem;
+`
+

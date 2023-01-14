aaf51fbff9f9b39182ded1b9d57163c3d709cee7
bae0203 <gmbae06@dgsw.hs.kr>
2023-01-14 20:24:09 +0900

    FIX :: MyInformation

--- src/layout/Myinfo.jsx
+++ src/layout/Myinfo.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Myinfo = () => {
+  const [nickname] = useState("");
   return (
     <>
-      <div>dd</div>
+      <div>사진</div>
+      <p>{nickname}</p>
+      <nav>
+        <span>
+          <span>팔로워</span>
+          <span>0</span>
+        </span>
+        <span>
+          <span>팔로우</span>
+          <span>3</span>
+        </span>
+      </nav>
+      <p>내 동영상</p>
+      <div></div>
     </>
   );
 };
